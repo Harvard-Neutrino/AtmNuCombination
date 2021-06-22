b5982fa4cb820072b3bbe170a50c93945ee4cefc
pabloferm <pablofer@liverpool.ac.uk>
2021-06-22 16:01:56 +0200

    Start migrating oscillator.

---
+++ src/Oscillator/AtmOscillator.py
@@ -0,0 +1,28 @@
+import matplotlib as mpl
+import nuSQuIDS as nsq
+import matplotlib.pyplot as plt
+import nuSQUIDSTools
+import numpy as np
+
+def SMOsc(nuSQ,flavor,in_state,th13,th23,th12,dm21,dm32,dcp,mh):
+# Mixing angles in radians
+	nuSQ.Set_MixingAngle(0,1,th12)
+	nuSQ.Set_MixingAngle(0,2,th13)
+	nuSQ.Set_MixingAngle(1,2,th23)
+	nuSQ.Set_SquareMassDifference(1,dm21)
+	nuSQ.Set_CPPhase(0,2,dcp)
+	nuSQ.Set_initial_state(in_state,nsq.Basis.flavor)
+
+	if mh==1:
+		nuSQ.Set_SquareMassDifference(2,dm32)
+	elif mh==-1:
+		nuSQ.Set_SquareMassDifference(2,mh*dm32+dm21)
+	else:
+		print('No valid MH value, setting MH to Normal')
+		nuSQ.Set_SquareMassDifference(2,dm32)
+
+	nuSQ.EvolveState()
+
+	j = int(abs(flavor) / 2) % 6
+
+	return nuSQ.EvalFlavor(j)
