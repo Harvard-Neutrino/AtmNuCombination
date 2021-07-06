328c97deb44394f3e7f029d973f48ee1bee86c6f
MiaochenJin <78837693+MiaochenJin@users.noreply.github.com>
2021-07-05 22:14:16 -0500

    Update analysis2.py

---
+++ sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/analysis2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import nuSQuIDS as nsq
+import nuflux
+
+matplotlib.rcParams.update({'font.size': 16})
+matplotlib.rcParams.update({'lines.linewidth': 3})
+matplotlib.rcParams.update({'patch.linewidth': 3})
