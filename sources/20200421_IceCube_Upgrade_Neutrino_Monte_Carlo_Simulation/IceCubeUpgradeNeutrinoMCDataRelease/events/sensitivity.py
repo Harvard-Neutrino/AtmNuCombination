48dfe7b820555055a9b35315c70ba371f2d9a467
MiaochenJin <78837693+MiaochenJin@users.noreply.github.com>
2021-07-28 13:19:19 -0400

    Update sensitivity.py

--- sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/sensitivity.py
+++ sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/sensitivity.py
@@ -45,22 +45,11 @@ numu_track_mask = numu_mask & track_mask
 energy_bins_fine = np.logspace(0., 2., num=21)
 energy_bins_course = np.logspace(0., 2., num=11)
 
-# List of theta13 and theta23 numeric values to probe
-t13l = [.2, .3, .4, .5, .6, .7, .8]
-t23l = [.5, .6, .7, .8, .9, 1, 1.1]
-
-# Prepare comparison values for flavor composition
-nuel_13 = []
-numul_13 = []
-nutaul_13 = []
-nuel_23 = []
-numul_23 = []
-nutaul_23 = []
-
 units = nsq.Const()
 
 interactions = False
 
+# Set propagation bins
 E_min = 10.0*units.GeV
 E_max = 1.0e3*units.PeV
 E_nodes = 100
@@ -73,83 +62,89 @@ cth_nodes = nsq.linspace(cth_min,cth_max,cth_nodes)
 
 neutrino_flavors = 3
 
+# theta23 numeric values to probe sensitivity
+t23min = 0.5
+t23max = 1.1
+t23step = 0.1
+t23l = np.arange(t23min, t23max + t23step, t23step)
 
-for t23 in t23l:
-    nsq_atm = nsq.nuSQUIDSAtm(cth_nodes,energy_nodes,neutrino_flavors,nsq.NeutrinoType.both,interactions)
+# Set the chi squared bins limits
+E_bin_min = 10 * units.GeV
+E_bin_max = 1000 * units.GeV
+E_n_bins = 10
 
-    AtmInitialFlux = np.zeros((len(cth_nodes),len(energy_nodes),2,neutrino_flavors))
-    flux = nuflux.makeFlux('honda2006')
-    for ic,cth in enumerate(nsq_atm.GetCosthRange()):
-        for ie,E in enumerate(nsq_atm.GetERange()):
-            nu_energy = E/units.GeV
-            nu_cos_zenith = cth
-            AtmInitialFlux[ic][ie][0][0] = flux.getFlux(nuflux.NuE,nu_energy,nu_cos_zenith) # nue
-            AtmInitialFlux[ic][ie][1][0] = flux.getFlux(nuflux.NuEBar,nu_energy,nu_cos_zenith) # nue bar
-            AtmInitialFlux[ic][ie][0][1] = flux.getFlux(nuflux.NuMu,nu_energy,nu_cos_zenith) # numu
-            AtmInitialFlux[ic][ie][1][1] = flux.getFlux(nuflux.NuMuBar,nu_energy,nu_cos_zenith) # numu bar
-            AtmInitialFlux[ic][ie][0][2] = flux.getFlux(nuflux.NuTau,nu_energy,nu_cos_zenith) # nutau
-            AtmInitialFlux[ic][ie][1][2] = flux.getFlux(nuflux.NuTauBar,nu_energy,nu_cos_zenith) # nutau bar
+# Set up chi squared bins
+bins = np.zeros_like(t23l.shape[0], E_n_bins)
 
-    nsq_atm.Set_MixingAngle(1, 2, t23)
-    nsq_atm.Set_initial_state(AtmInitialFlux,nsq.Basis.flavor)
-    nsq_atm.Set_ProgressBar(True) # progress bar will be printed on terminal
-    nsq_atm.EvolveState()
+# Set up parameters as in nu-fit5.0
+theta12 = 0.185778
+theta13 = 0.047611
+theta23 = 0.272222
 
-    lifetime = 365*24*60*60
-    meter_to_cm_sq = 1e4
-    rate_weight = np.zeros_like(input_data["weight"])
-    for i in range(len(rate_weight)):
-        if input_data["pdg"][i] > 0 :
-            neutype = 0
-        else:
-            neutype = 1
 
-        if np.abs(input_data["pdg"][i]) == 12:
-            neuflavor = 0
-        elif np.abs(input_data["pdg"][i]) == 14:
-            neuflavor = 1
-        elif np.abs(input_data["pdg"][i]) == 16:
-            neuflavor = 2
 
-        if input_data["true_energy"][i]*units.GeV < E_min or input_data["true_energy"][i]*units.GeV > E_max:
-            rate_weight[i] = 0
-            continue
-        rate_weight[i] = input_data["weight"][i]*nsq_atm.EvalFlavor(neuflavor,
-                                                                    np.cos(input_data["true_zenith"][i]),
-                                                                    input_data["true_energy"][i]*\
-                                                                    units.GeV,neutype)*lifetime*meter_to_cm_sq
 
-    input_data["rate_weight"] = rate_weight
-    nue = np.nansum(input_data["rate_weight"][nue_mask])
-    numu = np.nansum(input_data["rate_weight"][numu_mask])
-    nutau = np.nansum(input_data["rate_weight"][nutau_mask])
-    nuel_23.append(nue)
-    numul_23.append(numu)
-    nutaul_23.append(nutau)
-    
-print(nuel_23)
-print(numul_23)
-print(nutaul_23)
 
-chisq = np.array([0, 0, 0, 0, 0, 0, 0])
 
-def chisq(nuel, numul, nutaul):
-    for i in range(0,6):
-        res = (nuel[3] ** 2 - nuel[i] ** 2) / nuel[i] ** 2 \
-                    + (numul[3] ** 2 - numul[i] ** 2) / numul[i] ** 2 \
-                    + (nutaul[3] ** 2 - nutaul[i] ** 2) / nutaul[i] ** 2
-        print("HAHAHAHAHAH", res)
-        chisq[i] = res
 
-    
-fig,ax = plt.subplots(1)
+nsq_atm = nsq.nuSQUIDSAtm(cth_nodes,energy_nodes,neutrino_flavors,nsq.NeutrinoType.both,interactions)
 
-# create some x data and some integers for the y axis
-x = np.array(nuel_23)
+AtmInitialFlux = np.zeros((len(cth_nodes),len(energy_nodes),2,neutrino_flavors))
+flux = nuflux.makeFlux('honda2006')
+for ic,cth in enumerate(nsq_atm.GetCosthRange()):
+    for ie,E in enumerate(nsq_atm.GetERange()):
+        nu_energy = E/units.GeV
+        nu_cos_zenith = cth
+        AtmInitialFlux[ic][ie][0][0] = flux.getFlux(nuflux.NuE,nu_energy,nu_cos_zenith) # nue
+        AtmInitialFlux[ic][ie][1][0] = flux.getFlux(nuflux.NuEBar,nu_energy,nu_cos_zenith) # nue bar
+        AtmInitialFlux[ic][ie][0][1] = flux.getFlux(nuflux.NuMu,nu_energy,nu_cos_zenith) # numu
+        AtmInitialFlux[ic][ie][1][1] = flux.getFlux(nuflux.NuMuBar,nu_energy,nu_cos_zenith) # numu bar
+        AtmInitialFlux[ic][ie][0][2] = flux.getFlux(nuflux.NuTau,nu_energy,nu_cos_zenith) # nutau
+        AtmInitialFlux[ic][ie][1][2] = flux.getFlux(nuflux.NuTauBar,nu_energy,nu_cos_zenith) # nutau bar
 
-# plot the data
-ax.plot(x,chisq)
+nsq_atm.Set_MixingAngle(0, 1, 0.185778)
+nsq_atm.Set_MixingAngle(0, 2, 0.047611)
+nsq_atm.Set_MixingAngle(1, 2, theta23)
+nsq_atm.Set_SquaremassDifference(1, 7.42e-5)
+nsq_atm.Set_SquaremassDifference(2, 2.517e-3)
 
-fig.savefig(theta23_sensitivity.jpg)
+
+nsq_atm.Set_initial_state(AtmInitialFlux,nsq.Basis.flavor)
+nsq_atm.Set_ProgressBar(True) # progress bar will be printed on terminal
+nsq_atm.EvolveState()
+
+lifetime = 365*24*60*60
+meter_to_cm_sq = 1e4
+rate_weight = np.zeros_like(input_data["weight"])
+for i in range(len(rate_weight)):
+    if input_data["pdg"][i] > 0 :
+        neutype = 0
+    else:
+        neutype = 1
+
+    if np.abs(input_data["pdg"][i]) == 12:
+        neuflavor = 0
+    elif np.abs(input_data["pdg"][i]) == 14:
+        neuflavor = 1
+    elif np.abs(input_data["pdg"][i]) == 16:
+        neuflavor = 2
+
+    if input_data["true_energy"][i]*units.GeV < E_min or input_data["true_energy"][i]*units.GeV > E_max:
+        rate_weight[i] = 0
+        continue
+    rate_weight[i] = input_data["weight"][i]*nsq_atm.EvalFlavor(neuflavor,
+                                                                np.cos(input_data["true_zenith"][i]),
+                                                                input_data["true_energy"][i]*\
+                                                                units.GeV,neutype)*lifetime*meter_to_cm_sq
+
+input_data["rate_weight"] = rate_weight
+# Note that converting to mHz for the rate, as this is a more suitable unit for the IceCube Upgrade 
+fig, ax = plt.subplots(figsize=(7,6))
+fig.suptitle("Reconstructed Energy Rated Weight")
+ax.hist(input_data["reco_energy"], bins=energy_bins_fine, \
+      weights=input_data["rate_weight"][nue_cc_mask], \
+      label=r"$\nu_{e,CC}$", color="blue", histtype="step")
+
+
 
 
