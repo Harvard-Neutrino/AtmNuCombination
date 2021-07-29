d8f9a00c96a17666d4bb66ec87889a69b5083ae7
MiaochenJin <78837693+MiaochenJin@users.noreply.github.com>
2021-07-29 11:12:16 -0400

    Update sensitivity.py

--- sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/sensitivity.py
+++ sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/sensitivity.py
@@ -94,9 +94,8 @@ bins = np.zeros((t23l.shape[0], E_n_bins))
 theta12 = 0.185778
 theta13 = 0.047611
 theta23 = 0.272222
-
-
-
+m21 = 7.42e-5
+m31 = 2.517e-3
 
 nsq_atm = nsq.nuSQUIDSAtm(cth_nodes,energy_nodes,neutrino_flavors,nsq.NeutrinoType.both,interactions)
 
@@ -150,69 +149,137 @@ for i in range(len(rate_weight)):
 
 input_data["rate_weight"] = rate_weight
 
-# Plot the energy distribution
-# fig, ax = plt.subplots(figsize=(7,6))
-# fig.suptitle("Reco Energy Rated Distribution")
-# ax.hist(input_data["reco_energy"], bins=E_bin_plot, \
-#       weights=input_data["rate_weight"], \
-#       label=r"$\nu_{All}$", color="blue", histtype="step")
-# ax.set_xscale("log")
-fig_sns, ax_sns = plt.subplots(figsize=(7,6))
-fig_sns.suptitle("Reco Energy Rated Distribution")
-sns.histplot(data = input_data, x = "reco_energy", weights=input_data["rate_weight"], bins = energy_bins_fine.tolist(), binrange = (1, 100))
-ax_sns.set_xscale("log")
-ax_sns.set_xlabel(r"$E_{\nu,\rm{reco}}$ [GeV]")
-ax_sns.set_xlim(1, 100)
-ax_sns.ticklabel_format(axis='y', style='sci', scilimits=None,\
-                 useOffset=None, useLocale=None, useMathText=None)
-ax_sns.set_ylabel("Rate [Year]")
-ax_sns.grid(True)
-ax_sns.legend()
-fig_sns.savefig("Rate_For_Sensitivity.png")
-# ax.set_xlabel(r"$E_{\nu,\rm{reco}}$ [GeV]")
-# ax.set_xlim(1, 100)
-# ax.ticklabel_format(axis='y', style='sci', scilimits=None,\
-#                  useOffset=None, useLocale=None, useMathText=None)
-# ax.set_ylabel("Rate [Year]")
-# ax.grid(True)
-# ax.legend()
-# fig.savefig("Rate_For_Sensitivity.png")
+energy_hist_truth, energy_bins_truth = np.hist(input_data["reco_energy"], bins = energy_bins_fine, weights = input_data["rate_weight"])
 
-# Plot the angle distribution
-fig, ax = plt.subplots(figsize=(7,6))
-fig.suptitle("Reco Zenith Rated Distribution")
-ax.hist(np.cos(input_data["reco_zenith"]), bins=cos_bin_plot, \
-      weights=input_data["rate_weight"], \
-      label=r"$\nu_{All}$", color="blue", histtype="step")
-ax.set_xlabel(r"$\cos{\theta, \rm{reco}}$")
-ax.set_xlim(-1, 1)
-ax.ticklabel_format(axis='y', style='sci', scilimits=None,\
-                 useOffset=None, useLocale=None, useMathText=None)
-ax.set_ylabel("Rate [Year]")
-ax.grid(True)
-ax.legend()
-fig.savefig("Zenith_Rate_For_Sensitivity.png")
+def sanity_plots():
+    print("plotting sanity check distributions")
+    # Plot the energy distribution
+    # fig, ax = plt.subplots(figsize=(7,6))
+    # fig.suptitle("Reco Energy Rated Distribution")
+    # ax.hist(input_data["reco_energy"], bins=E_bin_plot, \
+    #       weights=input_data["rate_weight"], \
+    #       label=r"$\nu_{All}$", color="blue", histtype="step")
+    # ax.set_xscale("log")
+    fig_sns, ax_sns = plt.subplots(figsize=(7,6))
+    fig_sns.suptitle("Reco Energy Rated Distribution")
+    sns.histplot(data = input_data, x = "reco_energy", weights=input_data["rate_weight"], bins = energy_bins_fine.tolist(), binrange = (1, 100))
+    ax_sns.set_xscale("log")
+    ax_sns.set_xlabel(r"$E_{\nu,\rm{reco}}$ [GeV]")
+    ax_sns.set_xlim(1, 100)
+    ax_sns.ticklabel_format(axis='y', style='sci', scilimits=None,\
+                     useOffset=None, useLocale=None, useMathText=None)
+    ax_sns.set_ylabel("Rate [Year]")
+    ax_sns.grid(True)
+    ax_sns.legend()
+    fig_sns.savefig("Rate_For_Sensitivity.png")
+    # ax.set_xlabel(r"$E_{\nu,\rm{reco}}$ [GeV]")
+    # ax.set_xlim(1, 100)
+    # ax.ticklabel_format(axis='y', style='sci', scilimits=None,\
+    #                  useOffset=None, useLocale=None, useMathText=None)
+    # ax.set_ylabel("Rate [Year]")
+    # ax.grid(True)
+    # ax.legend()
+    # fig.savefig("Rate_For_Sensitivity.png")
 
-# Plot the overall distribution
-fig, ax = plt.subplots(figsize=(7,6))
-fig.suptitle("Reco Energy and Zenith Rated Distribution")
-# ax.hist2d(input_data["reco_energy"], np.cos(input_data["reco_zenith"]), bins=[energy_bins_fine, cos_bin_plot], \
-#       weights=input_data["rate_weight"], \
-#       label=r"$\nu_{All}$")
-# ax.set_xlabel(r"$E_{\nu,\rm{reco}}$ [GeV]")
-# ax.set_ylabel(r"$\cos{\theta, \rm{reco}}$")
-# ax.set_xlim(1, 100)
-# ax.set_xscale("log")
-# ax.set_ylim(-1, 1)
-# ax.legend()
-sns.histplot(data = input_data, x="reco_energy", y="reco_zenith", weights = "rate_weight", bins=(energy_bins_fine.tolist(), theta_bin_plot.tolist()), cbar=True)
-ax.set_xlabel(r"$E_{\nu,\rm{reco}}$ [GeV]")
-ax.set_ylabel(r"$\theta_{\rm{reco}}$")
-ax.set_xlim(1, 100)
-ax.set_xscale("log")
-ax.set_ylim(np.pi, 2 * np.pi)
-ax.legend()
-fig.savefig("2D_Rate_For_Sensitivity.png")
+    # Plot the angle distribution
+    fig, ax = plt.subplots(figsize=(7,6))
+    fig.suptitle("Reco Zenith Rated Distribution")
+    # ax.hist(np.cos(input_data["reco_zenith"]), bins=cos_bin_plot, \
+    #       weights=input_data["rate_weight"], \
+    #       label=r"$\nu_{All}$", color="blue", histtype="step")
+    sns.histplot(data = "input_data", x = "reco_zenith", weights = "rate_weight", bins = theta_bin_plot.tolist(), cbar = True)
+    ax.set_xlabel(r"$\cos{\theta, \rm{reco}}$")
+    # ax.set_xlim(-1, 1)
+    # ax.ticklabel_format(axis='y', style='sci', scilimits=None,\
+    #                  useOffset=None, useLocale=None, useMathText=None)
+    ax.set_ylabel("Rate [Year]")
+    ax.grid(True)
+    ax.legend()
+    fig.savefig("Zenith_Rate_For_Sensitivity.png")
+
+    # Plot the overall distribution
+    fig, ax = plt.subplots(figsize=(7,6))
+    fig.suptitle("Reco Energy and Zenith Rated Distribution")
+    # ax.hist2d(input_data["reco_energy"], np.cos(input_data["reco_zenith"]), bins=[energy_bins_fine, cos_bin_plot], \
+    #       weights=input_data["rate_weight"], \
+    #       label=r"$\nu_{All}$")
+    # ax.set_xlabel(r"$E_{\nu,\rm{reco}}$ [GeV]")
+    # ax.set_ylabel(r"$\cos{\theta, \rm{reco}}$")
+    # ax.set_xlim(1, 100)
+    # ax.set_xscale("log")
+    # ax.set_ylim(-1, 1)
+    # ax.legend()
+    sns.histplot(data = input_data, x="reco_energy", y="reco_zenith", weights = "rate_weight", bins=(energy_bins_fine.tolist(), theta_bin_plot.tolist()), cbar=True)
+    ax.set_xlabel(r"$E_{\nu,\rm{reco}}$ [GeV]")
+    ax.set_ylabel(r"$\theta_{\rm{reco}}$")
+    ax.set_xlim(1, 100)
+    ax.set_xscale("log")
+    ax.set_ylim(np.pi, 2 * np.pi)
+    ax.legend()
+    fig.savefig("2D_Rate_For_Sensitivity.png")
+
+sanity_plots()
 
 
+def get_energy_bins(theta23in, m31in):
+    
+    nsq_atm = nsq.nuSQUIDSAtm(cth_nodes,energy_nodes,neutrino_flavors,nsq.NeutrinoType.both,interactions)
 
+    AtmInitialFlux = np.zeros((len(cth_nodes),len(energy_nodes),2,neutrino_flavors))
+    flux = nuflux.makeFlux('honda2006')
+    for ic,cth in enumerate(nsq_atm.GetCosthRange()):
+        for ie,E in enumerate(nsq_atm.GetERange()):
+            nu_energy = E/units.GeV
+            nu_cos_zenith = cth
+            AtmInitialFlux[ic][ie][0][0] = flux.getFlux(nuflux.NuE,nu_energy,nu_cos_zenith) # nue
+            AtmInitialFlux[ic][ie][1][0] = flux.getFlux(nuflux.NuEBar,nu_energy,nu_cos_zenith) # nue bar
+            AtmInitialFlux[ic][ie][0][1] = flux.getFlux(nuflux.NuMu,nu_energy,nu_cos_zenith) # numu
+            AtmInitialFlux[ic][ie][1][1] = flux.getFlux(nuflux.NuMuBar,nu_energy,nu_cos_zenith) # numu bar
+            AtmInitialFlux[ic][ie][0][2] = flux.getFlux(nuflux.NuTau,nu_energy,nu_cos_zenith) # nutau
+            AtmInitialFlux[ic][ie][1][2] = flux.getFlux(nuflux.NuTauBar,nu_energy,nu_cos_zenith) # nutau bar
+
+    nsq_atm.Set_MixingAngle(0, 1, theta12)
+    nsq_atm.Set_MixingAngle(0, 2, theta13)
+    nsq_atm.Set_MixingAngle(1, 2, theta23in)
+    nsq_atm.Set_SquareMassDifference(1, m21)
+    nsq_atm.Set_SquareMassDifference(2, m31in)
+
+    nsq_atm.Set_initial_state(AtmInitialFlux,nsq.Basis.flavor)
+    nsq_atm.Set_ProgressBar(True) # progress bar will be printed on terminal
+    nsq_atm.EvolveState()
+
+    lifetime = 365*24*60*60
+    meter_to_cm_sq = 1e4
+    rate_weight = np.zeros_like(input_data["weight"])
+    for i in range(len(rate_weight)):
+        if input_data["pdg"][i] > 0 :
+            neutype = 0
+        else:
+            neutype = 1
+
+        if np.abs(input_data["pdg"][i]) == 12:
+            neuflavor = 0
+        elif np.abs(input_data["pdg"][i]) == 14:
+            neuflavor = 1
+        elif np.abs(input_data["pdg"][i]) == 16:
+            neuflavor = 2
+
+        if input_data["true_energy"][i]*units.GeV < E_min or input_data["true_energy"][i]*units.GeV > E_max:
+            rate_weight[i] = 0
+            continue
+        rate_weight[i] = input_data["weight"][i]*nsq_atm.EvalFlavor(neuflavor,
+                                                                    np.cos(input_data["true_zenith"][i]),
+                                                                    input_data["true_energy"][i]*\
+                                                                    units.GeV,neutype)*lifetime*meter_to_cm_sq
+    input_data["rate_weight"] = rate_weight
+    
+    # Now first obtain  the energy binned event rate distributions 1-100GeV
+    energy_hist, energy_bins = np.hist(input_data["reco_energy"], bins = energy_bins_fine, weight = input_data["rate_weight"])
+    
+    return energy_hist
+
+check = get_energy_bins(theta23, m31)
+print(energy_hist_truth)
+print(check)
+    
+    
