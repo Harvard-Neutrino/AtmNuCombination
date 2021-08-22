5ba6708c56b1899e9527ffa36b5040a165ea4498
MiaochenJin <jin_miaochen@outlook.com>
2021-08-22 19:42:03 -0400

    update

--- sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/run.py
+++ sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/run.py
@@ -1,10 +1,15 @@
 import plotting
+import sensitivity
 
 # plotting.distribution_plots()
-plotting.plot_t23_chi_raw_profile("t23_raw_profile_cascade", 0)
-plotting.plot_t23_chi_raw_profile("t23_raw_profile_track", 1)
-plotting.plot_t23_chi_raw_profile("t23_raw_profile_all", 2)
+# plotting.plot_t23_chi_raw_profile("t23_raw_profile_cascade", 0)
+# plotting.plot_t23_chi_raw_profile("t23_raw_profile_track", 1)
+# plotting.plot_t23_chi_raw_profile("t23_raw_profile_all", 2)
 # plotting.plot_m31_chi_raw_profile()
 # plotting.plot_contour_chi()
 # plotting.plot_t23_chi_raw_profile_all_top(savename = "t23_chi_sq_profile_raw_all_top")
 # plotting.plot_resolution()
+
+# For debug purposes
+sensitivity.get_chisq(theta23, m31, top = 2)
+
