d24ab9b9d9da693ef6fd14c443758102a1769a78
MiaochenJin <miaochenjin@boslogin04.rc.fas.harvard.edu>
2021-08-23 13:48:49 -0400

    update

--- sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/run.py
+++ sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/run.py
@@ -4,7 +4,8 @@ from params import *
 
 
 # plotting.distribution_plots()
-plotting.plot_t23_chi_raw_profile("t23_raw_profile_new_cascade", 0)
+#print("this is output file for both plot")
+#plotting.plot_t23_chi_raw_profile("t23_raw_profile_new_both", 2)
 # plotting.plot_t23_chi_raw_profile("t23_raw_profile_new_track", 1)
 # plotting.plot_t23_chi_raw_profile("t23_raw_profile_new_all", 2)
 # plotting.plot_m31_chi_raw_profile()
@@ -13,8 +14,12 @@ plotting.plot_t23_chi_raw_profile("t23_raw_profile_new_cascade", 0)
 # plotting.plot_resolution()
 
 # For debug purposes
-# print("chi-sq is", sensitivity.get_chisq(theta23, m31, top = 1))
-# sensitivity.get_rated_weight_truth(top = 1)
-# sensitivity.get_energy_bins(theta23, m31, top = 1)
-# print(sensitivity.get_t23_chi_profile(top = 2))
+top = 2
+print("for both")
+print("first we get the literature value:")
+rate_weight_truth, energy_hist_truth, energy_bins_truth = sensitivity.get_rated_weight_truth(top)
+print("chi-sq is", sensitivity.get_chisq(theta23, m31, energy_hist_truth, top))
+#sensitivity.get_rated_weight_truth(top = 1)
+#sensitivity.get_energy_bins(theta23, m31, top = 1)
+print(sensitivity.get_t23_chi_profile(top))
 
