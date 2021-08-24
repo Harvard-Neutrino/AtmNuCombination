31a487b71ff25bd270714c5bb9e43cccd2b53be7
MiaochenJin <jin_miaochen@outlook.com>
2021-08-23 21:24:44 -0400

    update

--- sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/sensitivity.py
+++ sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/sensitivity.py
@@ -207,3 +207,16 @@ def get_m31_chi_profile(t23 = theta23, top = 0):
 
 
 
+# plot un-normalized chisq for NH, probing values of t23, not minimizing over m31
+def plot_t23_chi_raw_profile(truth, savename = "t23_chi_sq_profile_raw_new", top = 0):
+    print("plotting t23 chi profile")
+    x = np.sin(t23l) ** 2
+    y = sst.get_t23_chi_profile(truth, top)
+    fig2, ax2 = plt.subplots(figsize=(7,6))
+    fig2.suptitle(r"$\theta_{23} \chi^2$ profile (raw)")
+    ax2.set_xlabel(r"$\sin^2{\theta_{23}}$")
+    ax2.set_ylabel(r"$\chi^2_{NH}$")
+    # ax2.set_yscale("log")
+    ax2.plot(x, y, color ="green")
+    ax2.grid(True)
+    fig2.savefig("{}.png".format(savename), bbox_inches='tight')
