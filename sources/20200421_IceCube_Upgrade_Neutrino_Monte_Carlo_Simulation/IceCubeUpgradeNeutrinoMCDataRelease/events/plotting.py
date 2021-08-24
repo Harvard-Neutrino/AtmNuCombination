e7a5d0c069f3495487505a7c212e044c52c0c0d8
MiaochenJin <jin_miaochen@outlook.com>
2021-08-24 03:10:21 -0400

    update

--- sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/plotting.py
+++ sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/plotting.py
@@ -76,7 +76,7 @@ def distribution_plots():
 # Plots contour of t23 and m31 chi-sq for NH
 def plot_contour_chi(savename = "chi_sq_contour", top = 0):
 	print("plotting chi squared contour")
-	rate_weight_truth, energy_hist_truth, energy_bins_truth = get_rated_weight_truth(top)
+	rate_weight_truth, energy_hist_truth, energy_bins_truth = sst.get_rated_weight_truth(top)
 
 	t23 = np.arange(t23min, t23max + t23step, t23step)
 	m31 = np.arange(m31min, m31max + m31step, m31step)
