1d8656c9954cbef6fd185e76b01c7a667f1f2384
MiaochenJin <miaochenjin@boslogin01.rc.fas.harvard.edu>
2021-10-03 23:06:38 -0400

    sync

--- sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/script.sh
+++ sources/20200421_IceCube_Upgrade_Neutrino_Monte_Carlo_Simulation/IceCubeUpgradeNeutrinoMCDataRelease/events/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -c 2                # Number of cores (-c)
-#SBATCH -t 2-15:30          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 7-00:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
 #SBATCH --mem-per-cpu=5000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
