098ba8da0d0ac708c8f9149b930686f245ce188d
MiaochenJin <jin_miaochen@outlook.com>
2021-10-13 14:53:43 -0400

    batchsubmit

---
+++ sources/batchsubmit/createfile.py
@@ -0,0 +1,29 @@
+import panda as pd
+
+# first define some parameters
+xlo = 0
+xhi = 10
+xstep = 1
+ylo = 0
+yhi = 10
+ystep = 1
+
+xlen = (xhi - xlo + 1)/xstep
+ylen = (yhi = ylo + 1)/ystep
+
+# create list of DM^2 for the dict
+x = []
+y = []
+
+for xi in range(xlen):
+    for yi in range(ylen):
+        x.append(xlo + xi * xstep)
+        y.append(ylo + yi * ystep)
+        yi += 1
+    xi += 1
+
+d = {'X' : x, 'Y' : y}
+
+df = pd.Dataframe(d)
+
+print(df.loc[df['X'] == 1])
