5562361476ab5c9131db6b5773dabff74d1871e4
pabloferm <pablofer@liverpool.ac.uk>
2022-01-19 16:27:29 +0100

    SK neutron tagging

--- src/Simulation/SuperK_wHtagging/run.sh
+++ src/Simulation/SuperK_wHtagging/run.sh
@@ -5,12 +5,12 @@
 #for i in data/input/atm_tau/gntp*hdf5; do python3 makeSimulation_HDF5.py $i; done
 
 # SK with H-neutron tagging
-for i in data/input/atm_emu/gntp*hdf5; do python3 makeSimulation_HDF5.py $i --H; done
-for i in data/input/atm_tau/gntp*hdf5; do python3 makeSimulation_HDF5.py $i --H; done
+#for i in data/input/atm_emu/gntp*hdf5; do python3 makeSimulation_HDF5.py $i --H; done
+#for i in data/input/atm_tau/gntp*hdf5; do python3 makeSimulation_HDF5.py $i --H; done
 
 # SK with Gd-neutron tagging
-for i in data/input/atm_emu/gntp*hdf5; do python3 makeSimulation_HDF5.py $i --Gd; done
-for i in data/input/atm_tau/gntp*hdf5; do python3 makeSimulation_HDF5.py $i --Gd; done
+#for i in data/input/atm_emu/gntp*hdf5; do python3 makeSimulation_HDF5.py $i --Gd; done
+#for i in data/input/atm_tau/gntp*hdf5; do python3 makeSimulation_HDF5.py $i --Gd; done
 
 # Merge all simulated files
 #python3 ../../../utils/HDF5merger.py data/output/SK data/output/SK/combined.hdf
@@ -18,7 +18,7 @@ python3 ../../../utils/HDF5merger.py data/output/SK_Htag data/output/SK_Htag/com
 python3 ../../../utils/HDF5merger.py data/output/SK_Gdtag data/output/SK_Gdtag/combined.hdf
 
 # Re-weighting after combination
-#python3 ammendWeight data/output/SK/combined.hdf
-python3 ammendWeight data/output/SK_Htag/combined.hdf
-python3 ammendWeight data/output/SK_Gdtag/combined.hdf
+#python3 ammendWeight.py data/output/SK/combined.hdf
+python3 ammendWeight.py data/output/SK_Htag/combined.hdf
+python3 ammendWeight.py data/output/SK_Gdtag/combined.hdf
 
