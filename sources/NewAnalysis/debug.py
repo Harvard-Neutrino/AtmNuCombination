34a148ab233e0f4bc571c18d14f488b5ae1cd20c
MiaochenJin <jin_miaochen@outlook.com>
2022-03-14 11:41:39 -0400

    new analysis

--- sources/NewAnalysis/debug.py
+++ sources/NewAnalysis/debug.py
@@ -17,16 +17,24 @@ analysis = cl.Analysis(sim, bf_fluxes, fluxes)
 util.get_all_weights(analysis, cl.PointType.BestFit)
 util.get_all_weights(analysis, cl.PointType.Physical)
 
-numevents = 0
-numnuevents = 0
+num_upgoing_events = 0
+num_downgoing_events = 0
+num_upgoing_nu_events = 0
+num_downgoing_nu_events = 0
 for i in range(2):
 	for j in range(2):
 		for k in range(2):
 			for l in range(len(analysis.bf_weights[i][j][k])):
-				if sim.E_tr[l] >= 1 and sim.E_tr[l] <= 53 and np.cos(sim.C_tr[l]) > 0:
-					numevents += analysis.bf_weights[i][j][k][l]
+				if sim.E_tr[l] >= 1 and sim.E_tr[l] <= 53 and np.cos(sim.C_tr[l]) < 0:
+					num_upgoing_events += analysis.bf_weights[i][j][k][l]
 					if sim.pdg[l] > 0:
-						numnuevents += analysis.bf_weights[i][j][k][l]
+						num_upgoing_nu_events += analysis.bf_weights[i][j][k][l]
+				elif sim.E_tr[l] >= 1 and sim.E_tr[l] <= 53 and np.cos(sim.C_tr[l]) > 0:
+					num_downgoing_events += analysis.bf_weights[i][j][k][l]
+					if sim.pdg[l] > 0:
+						num_downgoing_nu_events += analysis.bf_weights[i][j][k][l]
 
-print("total number of upgoing events is ", numevents)
-print("upgoing neutrinos is ", numnuevents)
+print("total number of upgoing events is ", num_upgoing_events)
+print("total number of upgoing neutrinos is ", num_upgoing_nu_events)
+print("total number of upgoing events is ", num_downgoing_events)
+print("total number of upgoing neutrinos is ", num_downgoing_nu_events)
