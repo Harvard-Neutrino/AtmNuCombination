6b46ae49436d762fdc75efbc07de7e10b76f6bc0
SantiagoGiner <santiagoginer@college.harvard.edu>
2023-11-25 09:05:47 -0500

    Revert "ORCA systematics from IC"
    This reverts commit e48e9ec5a4799b7e1e1cf0c320f4d2ee50ab4f15.

--- src/Analysis/Systematics/ORCADetector.py
+++ src/Analysis/Systematics/ORCADetector.py
@@ -19,63 +19,8 @@ def IntermediateClassification(x,experiment):
 	ev[experiment.Sample==1] = x
 	return experiment.Exp_wBinIt(ev) / experiment.weightOscBF_binned - 1
 
+
 def TrackClassification(x,experiment):
 	ev = np.ones(experiment.NumberOfEvents)
 	ev[experiment.Sample==2] = x
 	return experiment.Exp_wBinIt(ev) / experiment.weightOscBF_binned - 1
-
-def IceAbsorption_ORCA(x, experiment):
-	xx = x - 1
-	d = experiment.ExpFracNuECC * experiment.ice_absorption['nueCC'] + \
-	experiment.ExpFracNuMuCC * experiment.ice_absorption['numuCC'] + \
-	experiment.ExpFracNuTauCC * experiment.ice_absorption['nutauCC'] + \
-	experiment.ExpFracNC * experiment.ice_absorption['NC']
-	return xx * d
-
-def IceScattering_ORCA(x, experiment):
-	xx = x - 1
-	d = experiment.ExpFracNuECC * experiment.ice_absorption['nueCC'] + \
-	experiment.ExpFracNuMuCC * experiment.ice_absorption['numuCC'] + \
-	experiment.ExpFracNuTauCC * experiment.ice_absorption['nutauCC'] + \
-	experiment.ExpFracNC * experiment.ice_absorption['NC']
-	return xx * d
-
-def OffSet_ORCA(x, experiment):
-	xx = x
-	d = experiment.ExpFracNuECC * experiment.ice_absorption['nueCC'] + \
-	experiment.ExpFracNuMuCC * experiment.ice_absorption['numuCC'] + \
-	experiment.ExpFracNuTauCC * experiment.ice_absorption['nutauCC'] + \
-	experiment.ExpFracNC * experiment.ice_absorption['NC']
-	return xx * d
-
-def OptEffHeadon_ORCA(x, experiment):
-	xx = x
-	d = experiment.ExpFracNuECC * experiment.ice_absorption['nueCC'] + \
-	experiment.ExpFracNuMuCC * experiment.ice_absorption['numuCC'] + \
-	experiment.ExpFracNuTauCC * experiment.ice_absorption['nutauCC'] + \
-	experiment.ExpFracNC * experiment.ice_absorption['NC']
-	return xx * d
-
-def OptEffLateral_ORCA(x, experiment):
-	xx = x - 25
-	d = experiment.ExpFracNuECC * experiment.ice_absorption['nueCC'] + \
-	experiment.ExpFracNuMuCC * experiment.ice_absorption['numuCC'] + \
-	experiment.ExpFracNuTauCC * experiment.ice_absorption['nutauCC'] + \
-	experiment.ExpFracNC * experiment.ice_absorption['NC']
-	return xx * d
-
-def OptEffOverall_ORCA(x, experiment):
-	xx = x - 1
-	d = experiment.ExpFracNuECC * experiment.ice_absorption['nueCC'] + \
-	experiment.ExpFracNuMuCC * experiment.ice_absorption['numuCC'] + \
-	experiment.ExpFracNuTauCC * experiment.ice_absorption['nutauCC'] + \
-	experiment.ExpFracNC * experiment.ice_absorption['NC']
-	return xx * d
-
-def CoinFraction_ORCA(x, experiment):
-	xx = x
-	d = experiment.ExpFracNuECC * experiment.ice_absorption['nueCC'] + \
-	experiment.ExpFracNuMuCC * experiment.ice_absorption['numuCC'] + \
-	experiment.ExpFracNuTauCC * experiment.ice_absorption['nutauCC'] + \
-	experiment.ExpFracNC * experiment.ice_absorption['NC']
-	return xx * d
